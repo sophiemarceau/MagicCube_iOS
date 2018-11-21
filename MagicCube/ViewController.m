400f6b16b34c43123dafe2abfb0937b14228589e
wanmeizty <zhangtingyong@bte.top>
2018-11-21 11:49:34 +0800

    [task 3] 引入基本工具

--- MagicCube/ViewController.m
+++ MagicCube/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor= [UIColor redColor];
+    self.view.backgroundColor= [UIColor colorWithHexString:@"308cdd"];
+    
 }
 
 
