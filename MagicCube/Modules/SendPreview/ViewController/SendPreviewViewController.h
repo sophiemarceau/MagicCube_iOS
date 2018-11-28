94b8dbd2305db76d6664600728615c43e4f3aebc
sophiemarceau <sophiemarceau_qu@aliyun.com>
2018-11-28 23:38:02 +0800

    upate

---
+++ MagicCube/Modules/SendPreview/ViewController/SendPreviewViewController.h
@@ -0,0 +1,17 @@
+//
+//  SendPreviewViewController.h
+//  MagicCube
+//
+//  Created by sophiemarceau_qu on 2018/11/28.
+//  Copyright © 2018 wanmeizty. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SendPreviewViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MagicCube/Modules/SendPreview/ViewController/SendPreviewViewController.m
@@ -0,0 +1,26 @@
+
+//
+//  SendPreviewViewController.m
+//  MagicCube
+//
+//  Created by sophiemarceau_qu on 2018/11/28.
+//  Copyright © 2018 wanmeizty. All rights reserved.
+//
+
+#import "SendPreviewViewController.h"
+
+@interface SendPreviewViewController ()
+
+@end
+
+@implementation SendPreviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"发送预览";
+}
+
+
+
+
+@end
